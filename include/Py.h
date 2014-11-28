91b446408e5d3320fc79df882966100831e38836
Scott Prager <splinterofchaos@gmail.com>
2014-11-28 17:07:26 -0500

    Object: Add constructors for most PyTypes.

--- include/Py.h
+++ include/Py.h
@@ -8,6 +8,7 @@
 #include <type_traits>
 #include <utility>
 #include <string>
+#include <complex>
 
 namespace Py {
 
@@ -20,25 +21,46 @@ struct Object
     o = nullptr;
   }
 
-  explicit Object(PyObject *o, bool own=false) noexcept
-    : o(o)
-  {
+  explicit Object(PyObject *o, bool own=false) noexcept : o(o) {
     if (!own)
       incref();
   }
 
-  explicit Object(Object& obj) noexcept
-    : Object(obj.o)
-  {
+  explicit Object(Object& obj)  noexcept : Object(obj.o) { }
+  explicit Object(Object&& obj) noexcept : Object(obj.release(), true) { }
+  explicit Object(bool b)       noexcept : Object(b ? Py_True : Py_False) { }
+
+  // Strings
+  explicit Object(const char *s) noexcept { o = PyString_FromString(s); }
+  explicit Object(const std::string &s) noexcept : Object(s.c_str()) { }
+  explicit Object(const char *s, Py_ssize_t size) noexcept {
+    o = PyString_FromStringAndSize(s, size);
+  }
+  explicit Object(const std::string &s, Py_ssize_t size) noexcept
+    : Object(s.c_str(), size) {
   }
 
-  explicit Object(Object&& obj) noexcept
-    : Object(obj.release())
-  {
+  // Unicode
+  explicit Object(const Py_UNICODE *s, Py_ssize_t size) noexcept {
+    o = PyUnicode_FromUnicode(s, size);
   }
 
-  ~Object() noexcept
-  {
+  // Python numbers
+  Object(size_t x)             noexcept { o = PyInt_FromSize_t(x); }
+  Object(Py_ssize_t x)         noexcept { o = PyInt_FromSsize_t(x); }
+  Object(long long x)          noexcept { o = PyLong_FromLongLong(x); }
+  Object(unsigned long long x) noexcept { o = PyLong_FromUnsignedLongLong(x); }
+  Object(double x)             noexcept { o = PyFloat_FromDouble(x); }
+
+  // PyComplex
+  Object(double x, double y)   noexcept { o = PyComplex_FromDoubles(x, y); }
+  Object(Py_complex c)         noexcept { o = PyComplex_FromCComplex(c); }
+  template<typename T>
+  Object(const std::complex<T> &c)
+    : Object(std::real(c), std::imag(c)) {
+  }
+
+  ~Object() noexcept {
     decref();
   }
 
